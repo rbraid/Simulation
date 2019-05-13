2d7e42728f3b7a1153de3fd882aad2e2a666e51d
Ryan Braid <rbraid@mines.edu>
2019-05-13 11:49:50 -0400

    attempt to make the output file configurable

--- GRSIMaker/rungrsiMakerSelector.C
+++ GRSIMaker/rungrsiMakerSelector.C
@@ -1,10 +1,11 @@
-void rungrsiMakerSelector(TString fileloc)
+void rungrsiMakerSelector(TString fileloc, TString outloc = "SimCSMHits.root")
 {
   TFile *file = new TFile(fileloc,"read");
 
   TTree *tree = (TTree*)file->Get("ryandata");
 
   tree->Process("~/nuclear/be9/1010/GRSIMaker/grsiMakerSelector.C++");
-
+//   OutputFile = new TFile(outloc,"recreate");
+  
   gROOT->ProcessLine(".q");
 }	
