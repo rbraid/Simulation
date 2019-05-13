f47533aada322d4751a60e0d4ae9ca278917a54b
Ryan Braid <rbraid@mines.edu>
2019-05-13 11:53:57 -0400

    Deleted extra files

--- autorun.sh
+++
@@ -1,8 +0,0 @@
-cd ~/nuclear/be9/summed/
-
-make clean
-make -j 4 || ( echo -e "\n\n ********************** \n\n" ; make clean ; make ; paplay ~/Music/Powerup.ogg ; return 1 )
-9Be8Be run.mac exit.mac
-rt runryan.C || return 1
-rt finalrunryan.C
-cp final_event.root onandon/final_event_
--- starter.c
+++
@@ -1,7 +0,0 @@
-void starter()
-{
- //gROOT->ProcessLine(".L EventOut.root");
- //gROOT->ProcessLine("recondata->StartViewer()");
- gROOT->ProcessLine("actualdata->StartViewer()");
- //gSystem->Exec("paplay ~/Music/EnemyDeath.ogg");
-}
